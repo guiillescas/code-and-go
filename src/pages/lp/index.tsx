78b2ddefc2830072105d460cce862f52ad684129
Guilherme Illescas <gui.illescas@icloud.com>
2023-10-12 23:36:05 -0300

    feat: finished basic lp

---
+++ src/pages/lp/index.tsx
@@ -0,0 +1,129 @@
+import Image from 'next/image'
+import { useRouter } from 'next/router'
+import { ReactElement } from 'react'
+
+import Button from '@/components/Button'
+import { Boogaloo } from '@next/font/google'
+
+import * as Styles from './styles'
+
+const Font = Boogaloo({ subsets: ['latin'], weight: '400' })
+
+export default function LP(): ReactElement {
+  const router = useRouter()
+
+  function redirectToRegisterPage() {
+    router.push('/register')
+  }
+
+  return (
+    <Styles.LPContainer>
+      <main>
+        <nav>
+          <p className={Font.className}>Code&go</p>
+
+          <div>
+            <button
+              type="button"
+              className="secondary"
+              onClick={() => router.push('/login')}
+            >
+              Entrar
+            </button>
+            <button
+              type="button"
+              className="primary"
+              onClick={redirectToRegisterPage}
+            >
+              Cadastrar
+            </button>
+          </div>
+        </nav>
+
+        <section id="intro">
+          <div className="background one" />
+          <div className="background three" />
+          <div className="background four" />
+
+          <div className="left">
+            <h1>
+              Uma plataforma para você interagir com amigos enquanto aprende!
+            </h1>
+            <p>
+              Aqui você pode aprender programação diariamente, de uma forma
+              divertida. Ganhe pontos por exercícios finalizados corretamente e
+              fique no topo do ranking.
+            </p>
+          </div>
+
+          <div className="right">
+            <Image
+              src="/assets/tech_guy.svg"
+              alt="Imagem de um boneco ilustrado mexendo em um computador"
+              width={350}
+              height={350}
+            />
+          </div>
+        </section>
+
+        <section id="benefits">
+          <h2>No Code & Go, você vai:</h2>
+
+          <div className="item left">
+            <Image
+              src="/assets/rocket.svg"
+              alt="Ilustração de um foguete"
+              width={180}
+              height={180}
+            />
+
+            <div>
+              <h3>Evoluir rápido</h3>
+              <p>
+                Aqui focamos em prática. Resolva exercícios de forma prática e
+                objetiva. E melhor, os resultados saem na hora.
+              </p>
+            </div>
+          </div>
+          <div className="item right">
+            <div>
+              <h3>Interagir com amigos</h3>
+              <p>
+                Fique à frente de seus amigos acertando mais exercícios e
+                subindo no ranking!
+              </p>
+            </div>
+
+            <Image
+              src="/assets/friends.svg"
+              alt="Ilustração de amigos conversando"
+              width={180}
+              height={180}
+            />
+          </div>
+
+          <div className="item left">
+            <Image
+              src="/assets/options.svg"
+              alt="Ilustração de um foguete"
+              width={180}
+              height={180}
+            />
+
+            <div>
+              <h3>Aprender diversas linguagens</h3>
+              <p>
+                Aqui você escolhe a linguagem que quer aprender e exercitar.
+                Embarque nesse foguete e veja
+              </p>
+            </div>
+          </div>
+
+          <div className="callback-button-action">
+            <Button onClick={redirectToRegisterPage}>Quero me cadastrar</Button>
+          </div>
+        </section>
+      </main>
+    </Styles.LPContainer>
+  )
+}
---
+++ src/pages/lp/styles.ts
@@ -0,0 +1,231 @@
+import styled from 'styled-components'
+
+import Image1 from '../../../public/assets/blue-square-vector-1.png'
+import Image3 from '../../../public/assets/blue-vector-2.png'
+import Image2 from '../../../public/assets/purple-vector-1.png'
+
+export const LPContainer = styled.div`
+  width: 100%;
+  min-height: 100vh;
+
+  margin: 0 auto;
+
+  background: ${({ theme }) => theme.colors.white[500]};
+  color: ${({ theme }) => theme.colors.neutral[900]} !important;
+
+  main {
+    max-width: 1200px;
+    margin: 0 auto;
+
+    padding: 2rem 0 10rem;
+
+    > nav {
+      z-index: 6;
+
+      width: 100%;
+
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+
+      margin-bottom: 3rem;
+
+      > p {
+        font-size: 2rem;
+        text-decoration: none;
+        color: ${({ theme }) => theme.colors.neutral[900]};
+
+        z-index: 6;
+      }
+
+      > div {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        gap: 1rem;
+
+        z-index: 6;
+
+        button {
+          height: 36px;
+
+          border: none;
+          border-radius: 4px;
+
+          font-size: 16px;
+          font-weight: bold;
+          line-height: 20px;
+
+          cursor: pointer;
+
+          transition: opacity 0.2s;
+
+          padding: 0 1rem;
+
+          color: ${({ theme }) => theme.colors.white[500]};
+
+          &.primary {
+            background: ${({ theme }) => theme.colors.neutral[900]};
+
+            &:hover {
+              opacity: 0.8;
+            }
+          }
+          &.secondary {
+            background: transparent;
+            color: ${({ theme }) => theme.colors.neutral[900]};
+
+            transition: background-color 0.2s;
+
+            &:hover {
+              background: ${({ theme }) => theme.colors.white[500]};
+            }
+          }
+        }
+      }
+    }
+
+    > section {
+      padding: 0 2rem;
+    }
+
+    #intro {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      gap: 2rem;
+
+      position: relative;
+
+      height: calc(80vh - 2rem - 36px);
+
+      @media (max-width) {
+      }
+
+      > div.background {
+        position: absolute;
+
+        z-index: 2;
+
+        -webkit-filter: blur(100px);
+        -moz-filter: blur(100px);
+        -o-filter: blur(100px);
+        -ms-filter: blur(100px);
+        filter: blur(100px);
+
+        &.one {
+          top: -150px;
+          left: 580px;
+
+          width: 380px;
+          height: 380px;
+
+          background-image: url(${Image1.src});
+        }
+        &.two {
+          top: -120px;
+          right: -80px;
+
+          width: 2000px;
+          height: 380px;
+
+          background-image: url(${Image1.src});
+        }
+        &.three {
+          top: 200px;
+          left: 10%;
+
+          width: 700px;
+          height: 280px;
+
+          background-image: url(${Image2.src});
+        }
+        &.four {
+          top: 200px;
+          right: 10%;
+
+          width: 500px;
+          height: 280px;
+
+          background-image: url(${Image3.src});
+        }
+      }
+
+      .left {
+        z-index: 5;
+
+        > h1 {
+          font-size: 3rem;
+          max-width: 690px;
+        }
+
+        > p {
+          max-width: 600px;
+          margin-top: 1rem;
+          font-size: 1.25rem;
+        }
+      }
+
+      .right {
+        z-index: 6;
+      }
+    }
+
+    #benefits {
+      max-width: 768px;
+      margin: 0 auto;
+
+      > h2 {
+        text-align: center;
+        font-size: 2rem;
+
+        margin-bottom: 2.5rem;
+      }
+
+      .item {
+        display: flex;
+        align-items: center;
+        justify-content: flex-start;
+        gap: 1.5rem;
+
+        max-width: 500px;
+
+        &.right {
+          justify-content: flex-end;
+          margin-left: auto;
+        }
+
+        > div {
+          p {
+            margin-top: 1rem;
+          }
+        }
+      }
+
+      .callback-button-action {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+
+        margin-top: 3rem;
+
+        > button {
+          max-width: 300px;
+        }
+      }
+    }
+  }
+
+  #last-callback {
+    padding-top: 8rem;
+
+    background: ${({ theme }) =>
+      `linear-gradient(${theme.colors.white[500]}, ${theme.colors.neutral[900]})`};
+
+    > h4 {
+      text-align: center;
+
+      font-size: 2rem;
+    }
+  }
+`
